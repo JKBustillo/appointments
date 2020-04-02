b1d9ae6d52a9185b48b7374726d9e88ac4c5f01a
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-01 20:21:40 -0500

    Cita created and ready, conditional title added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import Form from './components/Form';
+import Cita from './components/Cita';
 
 function App() {
 
@@ -9,16 +10,31 @@ function App() {
   const crearCita = cita => {
     setCitas([...citas, cita]);
   }
+
+  // Función que elimina citas por id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    setCitas(nuevasCitas);
+  };
   
   return (
     <Fragment>
       <h1>Administrador de citas</h1>
       <div className="container">
-        <div className="one-half column">
-          <Form crearCita={crearCita} />
-        </div>
-        <div className="one-half column">
-          <h1>Ajá, cachón.</h1>
+        <div className="row">
+          <div className="one-half column">
+            <Form crearCita={crearCita} />
+          </div>
+          <div className="one-half column">
+            {citas.length === 0 ? <h2>No hay citas</h2> : <h2>Administra tus citas</h2>}
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </Fragment>
